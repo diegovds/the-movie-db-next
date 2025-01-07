71a59bbeee7191d07123b68d6e89d2e68bce84bc
Diego Viana <dsantos@inf.ufsm.br>
2025-01-07 16:03:40 -0300

    update homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,16 @@
+import { Movie } from '@/types/Movies'
 import Image from 'next/image'
-
-export interface MovieProps {
-  id: number
-  title: string
-  poster_path: string
-}
+import Link from 'next/link'
 
 interface ResponseProps {
-  results: MovieProps[]
+  results: Movie[]
 }
 
 const Home = async () => {
   const response = await fetch(
     `https://api.themoviedb.org/3/discover/movie?${process.env.THE_MOVIE_DB}`,
     {
+      cache: 'no-store',
       next: {
         revalidate: 0,
       },
@@ -22,9 +19,13 @@ const Home = async () => {
   const data: ResponseProps = await response.json()
 
   return (
-    <div className="m-auto grid w-max grid-cols-2 gap-10 md:grid-cols-3 lg:grid-cols-4">
+    <div className="mx-auto my-0 grid w-max grid-cols-2 gap-10 md:grid-cols-3 lg:grid-cols-4">
       {data.results.map((movie) => (
-        <div className="max-w-[150px] md:max-w-[200px]" key={movie.id}>
+        <Link
+          className="max-w-[150px] md:max-w-[200px]"
+          key={movie.id}
+          href={`/movie/${movie.id}`}
+        >
           <div className="relative h-[250px] md:h-[300px]">
             <Image
               src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
@@ -35,7 +36,7 @@ const Home = async () => {
             />
           </div>
           <h2 className="text-ellipsis text-center">{movie.title}</h2>
-        </div>
+        </Link>
       ))}
     </div>
   )
