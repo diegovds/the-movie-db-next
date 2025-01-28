34cc5b54df3e0da055d92c64cc15a33d9f300c85
Diego Viana <dsantos@inf.ufsm.br>
2025-01-28 16:46:31 -0300

    add font style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,15 @@
 import type { Metadata } from 'next'
+import { Poppins } from 'next/font/google'
 
 import Footer from './components/Footer'
 import Navbar from './components/Navbar'
 import './globals.css'
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  display: 'swap',
+  weight: '400',
+})
 
 export const metadata: Metadata = {
   title: 'The Movie BD NextJS',
@@ -29,7 +36,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="pt-BR">
+    <html lang="pt-BR" className={`${poppins.className} bg-gray-100`}>
       <body className="flex min-h-dvh flex-col antialiased">
         <Navbar />
         <main className="flex flex-1">{children}</main>
