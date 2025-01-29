f20d735e9d089fad676f14d63d1fb0f1e7fb4244
Diego Viana <dsantos@inf.ufsm.br>
2025-01-28 23:45:20 -0300

    add pagination in homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,23 @@
 import { Movie } from '@/types/Movies'
+import Link from 'next/link'
 import GridColumns from './components/GridColumns'
 import InfoCard from './components/InfoCard'
 
 interface ResponseProps {
   results: Movie[]
+  total_pages: number
+  total_results: number
 }
 
-const Home = async () => {
+type Props = {
+  searchParams: Promise<{ page: number | undefined }>
+}
+
+const Home = async ({ searchParams }: Props) => {
+  const { page } = await searchParams
+
   const response = await fetch(
-    `https://api.themoviedb.org/3/discover/movie?${process.env.THE_MOVIE_DB}`,
+    `https://api.themoviedb.org/3/discover/movie?${process.env.THE_MOVIE_DB}&include_adult=false&page=${page !== undefined && page > 0 ? page : 1}`,
     {
       cache: 'no-store',
       next: {
@@ -19,11 +28,21 @@ const Home = async () => {
   const data: ResponseProps = await response.json()
 
   return (
-    <GridColumns>
-      {data.results.map((movie) => (
-        <InfoCard key={movie.id} movie={movie} />
-      ))}
-    </GridColumns>
+    <div className="w-full">
+      <GridColumns>
+        {data.results.map((movie) => (
+          <InfoCard key={movie.id} movie={movie} />
+        ))}
+      </GridColumns>
+
+      <div className="mt-6 flex justify-center gap-2">
+        {Array.from({ length: 10 }).map((_, index) => (
+          <Link key={index} href={`/?page=${index + 1}`}>
+            {index + 1}
+          </Link>
+        ))}
+      </div>
+    </div>
   )
 }
 
