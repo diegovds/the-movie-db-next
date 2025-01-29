9b9583f04216597cfd27336193ad6eee88393102
Diego Viana <dsantos@inf.ufsm.br>
2025-01-29 17:43:11 -0300

    add person page

--- src/app/components/InfoCard.tsx
+++ src/app/components/InfoCard.tsx
@@ -1,6 +1,7 @@
 import { Movie } from '@/types/Movies'
+import { Person } from '@/types/Persons'
 import { Serie } from '@/types/Series'
-import { dateFormatting } from '@/utils/functions'
+import { dateFormatting, productionsList } from '@/utils/functions'
 import Image from 'next/image'
 import Link from 'next/link'
 import RatingCircle from './RatingCircle'
@@ -8,36 +9,54 @@ import RatingCircle from './RatingCircle'
 type InfoCardProps = {
   movie?: Movie
   serie?: Serie
+  person?: Person
 }
 
-const InfoCard = ({ movie, serie }: InfoCardProps) => {
+const InfoCard = ({ movie, serie, person }: InfoCardProps) => {
   return (
     <Link
       className="flex flex-col gap-2 overflow-hidden rounded-lg bg-gray-50 p-3 shadow-md"
-      href={movie ? `/movie/${movie.id}` : `/tv/${serie?.id}`}
+      href={
+        movie
+          ? `/movie/${movie.id}`
+          : serie
+            ? `/tv/${serie.id}`
+            : `/tv/${person?.id}`
+      }
     >
       <div className="relative h-[250px] overflow-hidden rounded-lg md:h-[300px]">
         <Image
           src={
             movie
               ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
-              : `https://image.tmdb.org/t/p/w500${serie?.poster_path}`
+              : serie
+                ? `https://image.tmdb.org/t/p/w500${serie.poster_path}`
+                : `https://image.tmdb.org/t/p/w500${person?.profile_path}`
           }
           alt="poster"
           priority
           quality={100}
           fill
         />
-        <RatingCircle
-          voteAverage={movie ? movie.vote_average : serie?.vote_average}
-        />
+        {(movie || serie) && (
+          <RatingCircle
+            voteAverage={movie ? movie.vote_average : serie?.vote_average}
+          />
+        )}
       </div>
       <h2 className="truncate text-center text-sm">
-        {movie ? movie.title : serie?.name}
+        {movie ? movie.title : serie ? serie.name : person?.name}
       </h2>
-      <data className="text-center text-xs text-gray-400">
-        {dateFormatting(movie ? movie.release_date : serie?.first_air_date)}
-      </data>
+      {(movie || serie) && (
+        <data className="text-center text-xs text-gray-400">
+          {dateFormatting(movie ? movie.release_date : serie?.first_air_date)}
+        </data>
+      )}
+      {person && (
+        <p className="text-center text-xs text-gray-400">
+          {productionsList(person.known_for)}
+        </p>
+      )}
     </Link>
   )
 }
--- src/app/person/page.tsx
+++ src/app/person/page.tsx
@@ -1,5 +1,42 @@
-const PersonPage = () => {
-  return <div>PersonPage</div>
+import { Person } from '@/types/Persons'
+import GridColumns from '../components/GridColumns'
+import InfoCard from '../components/InfoCard'
+
+interface ResponseProps {
+  results: Person[]
+  total_pages: number
+  total_results: number
+}
+
+type Props = {
+  searchParams: Promise<{ page: number | undefined }>
+}
+
+const PersonPage = async ({ searchParams }: Props) => {
+  const { page } = await searchParams
+
+  const response = await fetch(
+    `https://api.themoviedb.org/3/person/popular?${process.env.THE_MOVIE_DB}&include_adult=false&page=${page !== undefined && page > 0 ? page : 1}`,
+    {
+      cache: 'no-store',
+      next: {
+        revalidate: 0,
+      },
+    },
+  )
+  const data: ResponseProps = await response.json()
+
+  console.log(data.results)
+
+  return (
+    <div className="w-full">
+      <GridColumns>
+        {data.results.map((person) => (
+          <InfoCard key={person.id} person={person} />
+        ))}
+      </GridColumns>
+    </div>
+  )
 }
 
 export default PersonPage
