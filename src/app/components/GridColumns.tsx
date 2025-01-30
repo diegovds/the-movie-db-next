f4f5b5da38d210a60e7243f5578335223768b361
Diego Viana <dsantos@inf.ufsm.br>
2025-01-30 13:04:29 -0300

    add twMerge

--- src/app/components/GridColumns.tsx
+++ src/app/components/GridColumns.tsx
@@ -1,10 +1,17 @@
-type GridColumnsProps = {
-  children: React.ReactNode
-}
+import { cn } from '@/libs/utils'
+import { HTMLAttributes } from 'react'
 
-const GridColumns = ({ children }: GridColumnsProps) => {
+type GridColumnsProps = HTMLAttributes<HTMLDivElement>
+
+const GridColumns = ({ children, className, ...props }: GridColumnsProps) => {
   return (
-    <div className="mx-auto my-0 grid w-[97%] grid-cols-[repeat(auto-fill,_minmax(11.25rem,_1fr))] gap-5 md:w-[94%] md:gap-10">
+    <div
+      className={cn(
+        'mx-auto my-0 grid w-[97%] grid-cols-[repeat(auto-fill,_minmax(11.25rem,_1fr))] gap-5 md:w-[94%] md:gap-10',
+        className,
+      )}
+      {...props}
+    >
       {children}
     </div>
   )
