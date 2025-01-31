e695628cf4b621263dda9490b4839a5cfb04e4ff
Diego Viana <dsantos@inf.ufsm.br>
2025-01-31 15:34:14 -0300

    create SocialMedia component

---
+++ src/app/components/SocialMedia.tsx
@@ -0,0 +1,53 @@
+import { FaFacebook, FaImdb, FaInstagram } from 'react-icons/fa'
+import { FaSquareXTwitter } from 'react-icons/fa6'
+import Anchor from './Anchor'
+
+type SocialMediaProps = {
+  face: string
+  insta: string
+  x: string
+  imdb: string
+}
+
+const SocialMedia = ({ face, imdb, insta, x }: SocialMediaProps) => {
+  console.log(face)
+
+  return (
+    <div className="flex justify-center gap-6 md:justify-start">
+      {face !== null && (
+        <Anchor
+          href={`https://www.facebook.com/${face}`}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaFacebook />
+        </Anchor>
+      )}
+      {insta !== null && (
+        <Anchor
+          href={`https://www.instagram.com/${insta}`}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaInstagram />
+        </Anchor>
+      )}
+      {x !== null && (
+        <Anchor href={`https://x.com/${x}`} target="_blank" rel="noreferrer">
+          <FaSquareXTwitter />
+        </Anchor>
+      )}
+      {imdb !== null && (
+        <Anchor
+          href={`https://www.imdb.com/pt/title/${imdb}`}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaImdb />
+        </Anchor>
+      )}
+    </div>
+  )
+}
+
+export default SocialMedia
