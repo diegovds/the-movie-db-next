67535cde2a5dc39b099a4574400f983aea6b3c51
Diego Viana <dsantos@inf.ufsm.br>
2025-02-03 22:58:27 -0300

    Update Navbar.tsx

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -14,31 +14,31 @@ const Navbar = () => {
   }, [pathname])
 
   return (
-    <nav className="flex flex-col justify-between bg-slate-400 md:flex-row">
-      <div className="flex justify-between px-6 py-3">
-        <Link className="flex items-center" href={`/`}>
+    <nav className="flex flex-col justify-between bg-gray-800 px-6 py-1 md:flex-row md:py-2">
+      <div className="flex justify-between">
+        <Link className="flex items-center text-gray-300" href={`/`}>
           Icon
         </Link>
 
-        <div className="md:hidden">
+        <div className="text-gray-300 md:hidden">
           <Hamburger size={20} toggled={isOpen} toggle={setOpen} />
         </div>
       </div>
       <ul
-        className={`flex items-center gap-3 ${isOpen ? 'flex-col pb-3' : 'hidden flex-row pr-6 md:flex'}`}
+        className={`flex items-center gap-3 ${isOpen ? 'flex-col pb-3' : 'hidden flex-row md:flex'}`}
       >
         <li
-          className={`${pathname === '/' ? 'bg-white' : 'bg-slate-400'} rounded-lg p-1`}
+          className={`${pathname === '/' ? 'bg-gray-300' : 'text-gray-300'} rounded-lg p-1`}
         >
           <Link href={`/`}>Filmes</Link>
         </li>
         <li
-          className={`${pathname === '/tv' ? 'bg-white' : 'bg-slate-400'} rounded-lg p-1`}
+          className={`${pathname === '/tv' ? 'bg-gray-300' : 'text-gray-300'} rounded-lg p-1`}
         >
           <Link href={`/tv`}>Séries</Link>
         </li>
         <li
-          className={`${pathname === '/person' ? 'bg-white' : 'bg-slate-400'} rounded-lg p-1`}
+          className={`${pathname === '/person' ? 'bg-gray-300' : 'text-gray-300'} rounded-lg p-1`}
         >
           <Link href={`/person`}>Artistas</Link>
         </li>
