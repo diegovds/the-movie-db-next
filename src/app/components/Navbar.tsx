3d5494269c8673c661dfb093dfe38ba802fc017b
Diego Viana <dsantos@inf.ufsm.br>
2025-02-04 00:07:14 -0300

    add search form in navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,17 +1,37 @@
 'use client'
 
+import { zodResolver } from '@hookform/resolvers/zod'
 import { Rotate as Hamburger } from 'hamburger-react'
 import Link from 'next/link'
-import { usePathname } from 'next/navigation'
+import { usePathname, useRouter } from 'next/navigation'
 import { useEffect, useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { FaSearch } from 'react-icons/fa'
+import { z } from 'zod'
+
+const formSchema = z.object({
+  query: z.string(),
+})
+
+type FormData = z.infer<typeof formSchema>
 
 const Navbar = () => {
   const pathname = usePathname()
+  const router = useRouter()
   const [isOpen, setOpen] = useState(false)
+
+  const { handleSubmit, register, reset } = useForm<FormData>({
+    resolver: zodResolver(formSchema),
+  })
 
   useEffect(() => {
     setOpen(false)
-  }, [pathname])
+    reset()
+  }, [pathname, reset])
+
+  const onSubmit = ({ query }: FormData) => {
+    router.push(`/search/movie/${query}`)
+  }
 
   return (
     <nav className="flex flex-col justify-between bg-gray-800 px-6 py-1 md:flex-row md:py-2">
@@ -24,6 +44,20 @@ const Navbar = () => {
           <Hamburger size={20} toggled={isOpen} toggle={setOpen} />
         </div>
       </div>
+      <form className="flex gap-2" onSubmit={handleSubmit(onSubmit)}>
+        <input
+          type="text"
+          placeholder="Filme, série ou artista"
+          className="rounded-lg bg-gray-300 p-1 outline-none placeholder:text-gray-400"
+          {...register('query', { required: true })}
+        />
+        <button type="submit">
+          <FaSearch
+            className="h-full rounded-md bg-gray-300 px-1 transition-opacity hover:opacity-85"
+            size={22}
+          />
+        </button>
+      </form>
       <ul
         className={`flex items-center gap-3 ${isOpen ? 'flex-col pb-3' : 'hidden flex-row md:flex'}`}
       >
