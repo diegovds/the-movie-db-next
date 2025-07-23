1fe3f99b2129922e07651ad8c0cb4a411c84275c
Diego Viana <dsantos@inf.ufsm.br>
2025-07-23 05:01:09 -0300

    Add viewport settings to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from 'next'
+import type { Metadata, Viewport } from 'next'
 import { Poppins } from 'next/font/google'
 
 import Footer from './components/Footer'
@@ -28,6 +28,13 @@ export const metadata: Metadata = {
       follow: true,
     },
   },
+}
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
 }
 
 export default function RootLayout({
