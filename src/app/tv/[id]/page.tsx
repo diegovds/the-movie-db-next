579ea5844799c0c671738451890191bfbe7c712d
Diego Viana <dsantos@inf.ufsm.br>
2025-08-01 18:00:39 -0300

    update serie page

--- src/app/tv/[id]/page.tsx
+++ src/app/tv/[id]/page.tsx
@@ -49,7 +49,7 @@ const MoviePage = async ({ params }: Props) => {
   return (
     <div className="w-full flex-col">
       <InfoPage serie={data} />
-      <div className="m-6 flex flex-col md:flex-row">
+      <div className="my-3 flex flex-col md:flex-row">
         <div className="md:flex-[2]">
           <h2 className="mb-3 text-balance text-center text-2xl font-bold text-gray-100 md:text-wrap md:text-left md:text-3xl">
             Elenco principal
@@ -100,7 +100,7 @@ const MoviePage = async ({ params }: Props) => {
           </div>
         </div>
       </div>
-      <div className="m-6 mb-0">
+      <div className="my-3 mb-0">
         <TagH2 className="mb-3">Recomendações</TagH2>
         <GridColumns page={false} className="md:grid-cols-3 lg:grid-cols-5">
           {data.recommendations.results.slice(0, 10).map((recommendation) => (
